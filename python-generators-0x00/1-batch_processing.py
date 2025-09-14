4845950a6020d8df38cfd0950dd7ca5321b5917b
owuor <owuorvictorouma@gmail.com>
2025-09-14 15:20:37 +0300

    C:/Program Files/Git/1-batch_processing.cpython-313.pyc

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,26 +1,34 @@
-# from typing import List, Dict
-# from 0-stream_users import stream_users
-#
-#
-# def stream_users_in_batches(batch_size: int):
-#     """
-#     Generator that yields batches of users from the stream_users generator
-#     """
-#     batch: List[Dict] = []
-#     for user in stream_users():  # 1 loop
-#         batch.append(user)
-#         if len(batch) == batch_size:
-#             yield batch
-#             batch = []
-#     if batch:  # yield last incomplete batch
-#         yield batch
-#
-#
-# def batch_processing(batch_size: int):
-#     """
-#     Process batches and filter users over the age of 25
-#     """
-#     for batch in stream_users_in_batches(batch_size):  # 2nd loop
-#         for user in batch:  # 3rd loop
-#             if user["age"] > 25:
-#                 print(user)
+#!/usr/bin/python3
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    """
+    Generator function that fetches rows in batches from user_data table
+    """
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Victor",   # <-- replace with your MySQL password
+        database="ALX_prodev"
+    )
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM user_data")
+    while True:
+        rows = cursor.fetchmany(batch_size)  # fetch rows in batch
+        if not rows:
+            break
+        yield rows  # yield the whole batch
+
+    cursor.close()
+    conn.close()
+
+
+def batch_processing(batch_size):
+    """
+    Process each batch: filter users over the age of 25
+    """
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
