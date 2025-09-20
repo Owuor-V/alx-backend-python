ddfd057dac08059399b62c278b0577b45ab9b035
owuor <owuorvictorouma@gmail.com>
2025-09-20 19:59:48 +0300

    Ammendment to include django rest framework

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -69,6 +69,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
+
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 
